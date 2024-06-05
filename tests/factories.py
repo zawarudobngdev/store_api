e68f7d9532daf3b06e8e2e3c73b8af6574668125
Murilo Meranca <murilo.m@hotmail.com>
2024-06-05 12:28:52 -0300

    feat: add schemas with tests

---
+++ tests/factories.py
@@ -0,0 +1,2 @@
+def product_data():
+    return {"name": "Iphone XX", "quantity": 10, "price": "10.500", "status": True}
---
+++ tests/schemas/test_product.py
@@ -0,0 +1,29 @@
+from uuid import UUID
+from pydantic import ValidationError
+import pytest
+from store.schemas.product import ProductIn
+from tests.factories import product_data
+
+
+def test_schemas_return_success():
+    data = product_data()
+    product = ProductIn(**data)
+    # product = ProductIn.model_validate(data)
+
+    assert product.name == "Iphone XX"
+    assert isinstance(product.id, UUID)
+
+
+def test_schemas_return_raise():
+    data = {"name": "Iphone XX", "quantity": 10, "price": 10.500}
+
+    with pytest.raises(ValidationError) as err:
+        ProductIn.model_validate(data)
+
+    assert err.value.errors()[0] == {
+        "type": "missing",
+        "loc": ("status",),
+        "msg": "Field required",
+        "input": {"name": "Iphone XX", "quantity": 10, "price": 10.5},
+        "url": "https://errors.pydantic.dev/2.7/v/missing",
+    }
